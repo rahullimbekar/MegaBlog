99fca9a55bec65c74c06ba42ad72791a0910a198
rahullimbekar <rahullimbekar@gmail.com>
2023-11-06 00:47:54 +0530

    New project mega blog

---
+++ src/appwrite/auth.js
@@ -0,0 +1,59 @@
+import config from '../config/config';
+import { Client,Account,ID } from 'appwrite';
+
+export class AuthService {
+
+client=new Client();
+account;
+
+constructor (){
+    this.client
+        .setEndpoint(config.appWriteUrl)
+        .setProject(config.appWriteProjectId);
+    this.account=new Account(this.client);
+}
+
+async signUp({email,password,name}){
+    try {
+        const userAccount = await this.account.create(ID.unique(),email,password,name);
+        if (userAccount){
+            return this.signIn({email,password});
+        } else {
+            return userAccount;
+        }
+
+    } catch (error) {
+        console.log("Appwrite serive :: signUp :: error", error);
+    }
+}
+
+async signIn({email,password}){
+    try {
+        return await this.account.createEmailSession(email,password);
+    } catch (error) {
+        console.log("Appwrite serive :: signIn :: error", error);
+    }
+}
+
+async getCurrentUser() {
+    try {
+        return await this.account.get();
+    } catch (error) {
+        console.log("Appwrite serive :: getCurrentUser :: error", error);
+    }
+    return null;
+}
+
+async signOut(){
+    try {
+        await this.account.deleteSessions();
+    } catch (error) {
+        console.log("Appwrite serive :: signOut :: error", error);
+    }
+}
+
+}
+
+const authService=new AuthService();
+
+export default authService
